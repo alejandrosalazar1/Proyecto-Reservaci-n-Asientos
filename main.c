57507dcb8daab31404446abaeb8ec4f7d935f2cf
gsinibaldi <giancarlosinibaldi@ufm.edu>
2020-03-21 01:53:51 +0000

    Arreglos

--- main.c
+++ main.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+
 char asientos[32][6];
 int getMenuOption();
 void initializeSeats();
 void showSeats();
 
-int main;
-{
+
+int main(){
   int option = getMenuOption();
   printf("%d\n", option);
 }
 
 int getMenuOption(){
   int op;
-  printf("Bienvenido a la aerolinea \n");
+  printf("Bienvenido a la aerolinea: \n");
   printf("1. Reservar asiento \n");
-  printf("2. Ver disponibilidad de asientos \n")
-  printf("3. Salir \n")
+  printf("2. Ver disponibilidad de asientos \n");
+  printf("3. Salir \n");
   printf("Elija una opcion: \n");
 
-  scanf_s("%d", &op);
+  scanf("%d", &op);
   return op;
 }
 
-void init_asientos(){
+void initializeSeats(){
   for(int i = 0; i<32;i++){
     for(int j = 0; j<6;j++){
       asientos[i][j]= '0';
@@ -33,13 +33,13 @@ void init_asientos(){
   }
 }
 
-void mostrar_asientos() {
+void showSeats() {
   printf("|     | A | B | C | E | F |\n");
   printf("___________________________\n");
   for(int i=0; i<32; i++){
-    printf("| %2d | %c | %c | %c | %c | %c | %c |\n", asientos[i][0],asientos[i][1],asientos[i][2],asientos[i][3],asientos[i][4],asientos[i][5]);
-    i+1
+    printf("| %2d | %c | %c | %c | %c | %c | %c |\n", i+1,asientos[i][0],asientos[i][1],asientos[i][2],asientos[i][3],asientos[i][4],asientos[i][5]);
+    
   }
 }
-}
+
 
