7bbc25f7763c14351cd177c1e3e42c76969217a1
gsinibaldi <giancarlosinibaldi@ufm.edu>
2020-03-21 00:56:31 +0000

    menu

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main(){
+  int opcion;
+  char numero_vuelo[5];
+  bienvenida:
+  printf("Bienvenido, ingrese numero de vuelo:\n");
+  scanf("%c", &numero_vuelo);
+  
+  if ((strlen(numero_vuelo)==5)){
+    goto bienvenida; 
+    
+  } else { 
+    while(opcion !=4){
+    printf("====================\n");
+    printf("1. Reservar asientos \n 2. Ver asientos disponibles \n 3. Ver Resumen \n 4. Salir \n ¿Que desea realizar? \n-> ");
+  }
+  }
+  
 }
+
