9e08d485451c5f9e269cfb758b629a53b205603b
gsinibaldi <giancarlosinibaldi@ufm.edu>
2020-03-21 01:32:07 +0000

    Inicializar asientos

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int getMenuOption();
 void initializeSeats();
 void showSeats();
 
-int main
+int main;
 {
   int option = getMenuOption();
   printf("%d\n", option);
@@ -23,5 +23,23 @@ int getMenuOption(){
 
   scanf_s("%d", &op);
   return op;
+}
+
+void init_asientos(){
+  for(int i = 0; i<32;i++){
+    for(int j = 0; j<6;j++){
+      asientos[i][j]= '0';
+    }
+  }
+}
+
+void mostrar_asientos() {
+  printf("|     | A | B | C | E | F |\n");
+  printf("___________________________\n");
+  for(int i=0; i<32; i++){
+    printf("| %2d | %c | %c | %c | %c | %c | %c |\n", asientos[i][0],asientos[i][1],asientos[i][2],asientos[i][3],asientos[i][4],asientos[i][5]);
+    i+1
+  }
+}
 }
 
